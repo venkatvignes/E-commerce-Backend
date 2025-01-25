55fcb02d46d59e3466cc8a612796c34e6cfa4a74
Venkat <venkateshv.vijayan@gmail.com>
2025-01-25 21:39:25 +0530

    chnages added

--- Models/userSchema.js
+++ Models/userSchema.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true, // Password is mandatory
-      minlength: 6, // Minimum password length
+      minlength: 10,
     },
     role: {
       type: String,
